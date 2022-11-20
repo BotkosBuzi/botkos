0a5fe2f2126a88c3b5c90554bc175b8b9be1f86c
Neutron17 <neutron17771@protonmail.com>
2022-11-20 21:00:30 +0100

    stuff & signiture

--- js/settings.js
+++ js/settings.js
@@ -1,3 +1,10 @@
+const slider = document.getElementById("myRange");
+
 function back() {
     window.open("index.html", "_self");
+}
+
+function save() {
+    console.assert(slider != null)
+    alert(slider.innerHTML)
 }
